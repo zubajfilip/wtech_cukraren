36cfd80099283ff1f3c7eae74855b704fba95a64
ErMiNik <milanmikulas123@gmail.com>
2024-04-16 15:40:00 +0200

    add login, reg endpoints

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\DonutController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
 
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::get('/penis', function () {
     return view('penis');
@@ -17,10 +19,18 @@ Route::get('/penis', function () {
 
 Route::resource('users', UserController::class);
 
-Route::resource('home', ProductController::class);
+Route::resource('home', ProductController::class)->names([
+    'index' => 'home'
+]);
 
 Route::resource('donuts', DonutController::class)->names([
     'index' => 'donuts'
-]);;
+]);
 
-// Route::get('donuts', 'DonutController@index')->name('donuts');
+Route::resource('login', LoginController::class)->names([
+    'index' => 'login'
+]);
+
+Route::resource('register', RegisterController::class)->names([
+    'index' => 'register'
+]);
