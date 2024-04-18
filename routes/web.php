ca969e511bf89a89ae77d9ac9c3e1130fd547038
ErMiNik <milanmikulas123@gmail.com>
2024-04-18 16:33:58 +0200

    add cart endpoints

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\DonutController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Cart1Controller;
+use App\Http\Controllers\Cart2Controller;
+use App\Http\Controllers\Cart3Controller;
 
 
 
@@ -33,4 +36,16 @@ Route::resource('login', LoginController::class)->names([
 
 Route::resource('register', RegisterController::class)->names([
     'index' => 'register'
+]);
+
+Route::resource('cart1', Cart1Controller::class)->names([
+    'index' => 'cart1'
+]);
+
+Route::resource('cart2', Cart2Controller::class)->names([
+    'index' => 'cart2'
+]);
+
+Route::resource('cart3', Cart3Controller::class)->names([
+    'index' => 'cart3'
 ]);
