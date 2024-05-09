1fbea1df0dde2319b3a9413fef6b67b6622db912
zubajfilip <113968752+zubajfilip@users.noreply.github.com>
2024-05-09 19:25:11 +0200

    opraveny search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,18 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
     public function search(Request $Request)
     {
         $user = Auth::user();
-        $searchTerm = $Request->input('search'); // Get the search query from the request
+        $searchTerm = $Request->input('search');
 
-        // Perform the search on the Product model
-        $products = Product::where('name', 'ILIKE', "%$searchTerm%")->get();
 
-        // Return the search results to the view (details in step 3)
+
+
+        $sql = "SELECT * 
+        FROM products 
+        WHERE unaccent(name) ILIKE unaccent('%' || ? || '%')";
+        
+        $products = DB::select($sql, [$searchTerm]);
+        
+        //dd($products);
+        // Return the search results to the view
         return view('search-results', compact('products'))->with('user', $user);
     }
 }
