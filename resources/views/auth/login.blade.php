ec0bbf855e9ce58f082638cec0b9464aaa458184
zubajfilip <113968752+zubajfilip@users.noreply.github.com>
2024-05-10 13:08:40 +0200

    Upravene logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Heslo')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -28,7 +28,7 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Zapamätaj si ma') }}</span>
             </label>
         </div>
 
@@ -40,7 +40,7 @@
             @endif
 
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('Prihlásiť sa') }}
             </x-primary-button>
         </div>
     </form>
