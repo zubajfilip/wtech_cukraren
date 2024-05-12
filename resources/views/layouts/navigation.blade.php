4c928ee5f55452cb070b505e46eb5e1f80b640a3
zubajfilip <113968752+zubajfilip@users.noreply.github.com>
2024-05-12 15:41:31 +0200

    uprava profil headeru

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav x-data="{ open: false }" class="bord-col">
+<nav x-data="{ open: false }" class="upper-part">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 upper-part">
-        <div class="flex justify-between h-16">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16 upper-part">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
