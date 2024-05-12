7de117a1ac907770bbc014df5e153e605e828263
ErMiNik <milanmikulas123@gmail.com>
2024-05-12 08:20:02 +0200

    move script to footer component

--- resources/views/carts/final.blade.php
+++ resources/views/carts/final.blade.php
@@ -12,9 +12,6 @@
         <h2>Vaša objednávka bola úspešne spracovaná</h2>
     </div>
     @include('components.footer')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,3 +6,7 @@
         <p class="contact-item mb-0">📍 Ilkovicova 2, 842 16 Karlova Ves</p>
     </div>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
